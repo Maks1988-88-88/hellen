fa32d076099c3ed2e7df7edafd92f826d57c6919
Dmytro Marchenko <arif007@ukr.net>
2021-01-29 22:03:48 +0200

    vk + time

--- src/js/slide.js
+++ src/js/slide.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     speed: 300,
     arrows: false,
       infinite: true,
+      autoplay: true,
+  autoplaySpeed: 7000,
        focusOnSelect: true,
       asNavFor: '.slider-second',
     responsive: [
