f40565900cb9f8a0cb49b1bbc8fc81c8837690f1
Dmytro Marchenko <arif007@ukr.net>
2021-01-29 17:01:48 +0200

    1

---
+++ src/js/slide.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $('.rev_slider').slick({
+         dots: false,
+    speed: 300,
+    arrows: false,
+      infinite: true,
+       focusOnSelect: true,
+      asNavFor: '.slider-second',
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+   arrows: false,
+        }
+      }
+       ]
+   
+  });
+  $('.slider-second').slick({
+    slidesToShow: 6,
+  slidesToScroll: 1,
+    asNavFor: '.rev_slider',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    variableWidth:false,
+    arrows: true,
+    infinite: true,
+    
+    
+
+    responsive: [
+    {
+      breakpoint: 767,
+        settings: {
+        slidesToShow: 3,
+  slidesToScroll: 1,
+    arrows: true,
+        }
+      }
+       ]
+
+  });
+  
+
+});
